5b7c6a038ec417c20a4aefdb64149921649b57a7
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-09-14 17:13:45 +0200

    Add legally required links

--- js/pages/main/account.js
+++ js/pages/main/account.js
@@ -12,6 +12,8 @@ export default {
       <li><router-link to="/account/backup-and-restore"><span class="material-icons-round">settings_backup_restore</span>Backup And Restore</router-link></li>
       <li><router-link to="/account/help"><span class="material-icons-round">help</span>Help</router-link></li>
       <li><router-link to="/account/about"><span class="material-icons-round">info</span>About</router-link></li>
+      <li><a rel="nofollow noopener noreferrer" target="_blank" href="https://anvil-solutions.com/en/imprint"><span class="material-icons-round">policy</span>Imprint</a></li>
+      <li><a rel="nofollow noopener noreferrer" target="_blank" href="https://anvil-solutions.com/en/privacy"><span class="material-icons-round">policy</span>Privacy</a></li>
     </ul>
   </page-tab-bar>`,
   components: {
